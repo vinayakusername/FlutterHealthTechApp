9cb86c5dee55453b6a2f690271c5e29a1fd309f7
vinayakusername <gpraut88@gmail.com>
2020-05-30 15:58:11 +0530

    files Updated

---
+++ lib/Views/widget_searchBarPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchBarPage extends StatefulWidget {
+  @override
+  _SearchBarPageState createState() => _SearchBarPageState();
+}
+
+class _SearchBarPageState extends State<SearchBarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
