71a426576444e95abca5b2eced1ab93d599286ed
vinayakusername <gpraut88@gmail.com>
2020-06-16 11:23:14 +0530

    files updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,49 @@
+
 import 'package:flutter/material.dart';
-import 'package:health_tech_app1/Views/widget_searchBarPage.dart';
+import 'package:flutter/services.dart';
+import 'package:health_tech_app1/helper/authenticate.dart';
+import 'package:health_tech_app1/helper/constants.dart';
+import 'package:health_tech_app1/views/home.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-       
-        primarySwatch: Colors.pink
-      ),
-      //home: LoginRegisterPage(),
-
-      home: SearchBarPage(),
-    );
-      
-     
-  }
+void main() {
+  runApp(MyApp());
 }
 
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+  bool isUserLoggedIn = false;
+
+  @override
+  void initState() {
+    getLoggedInState();
+    super.initState();
+  }
+
+  getLoggedInState() async {
+    await Constants.getUerLoggedInSharedPreference().then((value) {
+      setState(() {
+        isUserLoggedIn = value;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'Quiz App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.pink
+        //visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: isUserLoggedIn ? Home() : Authenticate(),
+    );
+  }
+}
