f8229930062339235f2b940e33305ffc8fd9d005
vinayakusername <gpraut88@gmail.com>
2020-06-26 16:24:05 +0530

    files updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:health_tech_app1/Views/widget_searchBarPage.dart';
+import 'package:health_tech_app1/Views/widget_HomePage.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -14,9 +15,9 @@ class MyApp extends StatelessWidget {
        
         primarySwatch: Colors.pink
       ),
-      //home: LoginRegisterPage(),
-
-      home: SearchBarPage(),
+      
+      //home: SearchBarPage(),
+      home: MyHomePage(),
     );
       
      
