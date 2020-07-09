cdfb4e089631d6827cd2892fea7a0cfbb4e9b2d5
vinayakusername <gpraut88@gmail.com>
2020-07-09 16:06:32 +0530

    new images added in assets

--- lib/Views/home_gallery_sub_page.dart
+++ lib/Views/home_gallery_sub_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 //import 'package:health_tech_app1/Views/home_tutorial_sub_page.dart';
 
@@ -9,28 +11,47 @@ class HomeGallerySubPage extends StatefulWidget {
 class _HomeGallerySubPageState extends State<HomeGallerySubPage> {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView
+    return FutureBuilder
     (
-       slivers: <Widget>
-       [
-         //HomeSliverAppBar('Gallery'),
-         _buildSliverContent()
-       ],
+      future: _loadImagePaths(context),
+      builder: (BuildContext context,AsyncSnapshot <List<String>> imagePathsSnapshot)
+      {
+        if(imagePathsSnapshot.connectionState==ConnectionState.done && imagePathsSnapshot.hasData)
+        {
+          return _buildContent(imagePathsSnapshot.data);
+        }
+        return Center(child: CircularProgressIndicator());
+      },
+      
     );
   }
 
-  SliverFillRemaining _buildSliverContent()
+  GridView _buildContent(List<String>imagePaths)
   {
-    return SliverFillRemaining
-    (
-      child: Center
-      (
-        child: Text
-        (
-          'Gallery',
-           style:TextStyle(fontSize: 20,fontWeight: FontWeight.bold)
-        ),
-      ),
-    );
+     return GridView.count
+     (
+       crossAxisCount: 2,
+       children: imagePaths
+       .map<Widget>((String imagePath)=>_buildImageWidget(imagePath))
+       .toList()
+     );
   }
+   //This method is used to load the all images at a time from assets folder
+   Future<List<String>> _loadImagePaths(BuildContext context) async {
+      final String manifestContentJson = await DefaultAssetBundle
+                                                 .of(context)
+                                                 .loadString('AssetsManifest.json');
+
+      final Map<String,dynamic> manifestMap = json.decode(manifestContentJson);
+      return manifestMap.keys.where((String key)=>key.contains('assets/images/')).toList();
+   }
+
+   Image _buildImageWidget(String imagePath)
+   {
+     return Image.asset
+     (
+       imagePath,
+       fit: BoxFit.cover
+     );
+   }
 }
